324c3fda90e6184e08e92d8701d92b4a86dc319a
Chojecki <mar.chojecki@gmail.com>
2021-08-30 20:47:21 +0200

    first step

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like
+// pointers to database connections
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Run the app")
+	return nil
+}
+
 func main() {
-	fmt.Println("Go go go")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting the app")
+		fmt.Println(err)
+	}
 }
