f041bafa8f2d7c0200a1ead34415737293067ed2
Chojecki <mar.chojecki@gmail.com>
2021-08-31 23:50:33 +0200

    implementing comments interface

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -19,7 +19,7 @@ type Comment struct {
 }
 
 type CommentService interface {
-	GetComment(ID uint) (*Comment, error)
+	GetComment(ID uint) (Comment, error)
 	PostComment(comment Comment) (Comment, error)
 	UpdateComment(ID uint, newComment Comment) (Comment, error)
 	DeleteComment(ID uint) error
@@ -30,4 +30,55 @@ func NewService(db *gorm.DB) *Service {
 	return &Service{
 		DB: db,
 	}
+}
+
+func (s *Service) GetComment(ID uint) (Comment, error) {
+	var comment Comment
+	if result := s.DB.First(&comment, ID); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+func (s *Service) GetCommentsBySlug(slug string) ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments).Where("slug = ?", slug); result.Error != nil {
+		return []Comment{}, result.Error
+	}
+	return comments, nil
+}
+
+func (s *Service) PostComment(comment Comment) (Comment, error) {
+	if result := s.DB.Save(&comment); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+func (s *Service) UpdateComment(ID uint, newComment Comment) (Comment, error) {
+	comment, err := s.GetComment(ID)
+	if err != nil {
+		return Comment{}, err
+	}
+
+	if result := s.DB.Model(&comment).Update(newComment); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+func (s *Service) DeleteComment(ID uint) error {
+
+	if result := s.DB.Delete(&Comment{}, ID); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (s *Service) GetAllComments() ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments); result.Error != nil {
+		return []Comment{}, result.Error
+	}
+	return comments, nil
 }
